3ffe1ecc2731de4014d3dcb1c4e787e9753f711e
PrabhatGurung <gurungprabhat777@gmail.com>
2024-12-30 12:03:10 -0500

    CH-10:Repository

--- Repository/StockRepository.cs
+++ Repository/StockRepository.cs
@@ -2,11 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using asp.net.Data;
+using asp.net.Interfaces;
+using asp.net.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace asp.net.Repository
 {
-    public class StockRepository
+    public class StockRepository : IStockRepository
     {
-        
+        private readonly ApplicationDBContext _context;
+        public StockRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public Task<List<Stock>> GetAllAsync()
+        {
+            return _context.Stock.ToListAsync();
+        }
+
     }
+    
+    
+        
 }
